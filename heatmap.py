874b0cd18180af5bdfdef98f7ba7ad26cfcf231a
JOSEPH NJOROGE <snowngich35@gmail.com>
2024-10-23 13:45:33 +0300

    updates

--- heatmap.py
+++ heatmap.py
@@ -1,6 +1,6 @@
 
 import matplotlib.pyplot as plt
-import numpy as np
+import nu as np
 data = np.random.rand(10 , 12)
 sns.heatmap(data, cmap='viridis')
 plt.show()
---
+++ matplotlib.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import nu as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, color = 'r')
+plt.show()
